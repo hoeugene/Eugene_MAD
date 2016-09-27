da0797eb29a3f9a31d712a96658c8e04ca454f53
hoeugene <hodominic@reshut1-62-112-dhcp.int.colorado.edu>
2016-09-27 17:18:16 -0600

    update 1
    app icon and launch screen

---
+++ lab 4/lab 4Tests/lab_4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  lab_4Tests.swift
+//  lab 4Tests
+//
+//  Created by Eugene Ho on 27/9/2016.
+//  Copyright © 2016 Eugene Ho. All rights reserved.
+//
+
+import XCTest
+@testable import lab_4
+
+class lab_4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
