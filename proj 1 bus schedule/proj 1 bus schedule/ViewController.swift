54cc616801b00a6c40d37e843da6baf11dbb069a
hoeugene <hodominic@rgnt2-124-150-dhcp.int.colorado.edu>
2016-09-29 11:50:55 -0600

    project 1 milestone 2
    Justin mind prototype and photos

---
+++ proj 1 bus schedule/proj 1 bus schedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proj 1 bus schedule
+//
+//  Created by Eugene Ho on 28/9/2016.
+//  Copyright © 2016 Eugene Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
