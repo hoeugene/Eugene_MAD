e5b68d5ac39e7581df333107b5888ff7876b590d
hoeugene <hodominic@Not-your-macbook-air.local>
2016-11-09 20:30:52 -0700

    Android Lab 7

--- Andriod/Feelings/app/src/main/java/com/example/eugene/feelings/MainActivity.java
+++ Andriod/Feelings/app/src/main/java/com/example/eugene/feelings/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         EditText name = (EditText)findViewById(R.id.nameEditText);
         String nameValue = name.getText().toString();
         TextView feeling = (TextView)findViewById(R.id.feelingTextView);
+
         // Spinner
         Spinner moodSpinner = (Spinner)findViewById(R.id.spinner);
         String spinnerValue = String.valueOf(moodSpinner.getSelectedItem());
@@ -32,16 +35,16 @@ public class MainActivity extends AppCompatActivity {
         boolean energy = toggle.isChecked();
         String energyText = "empty";
         if(energy) {
-            energyText = "positive ";
+            energyText = " positive ";
         } else {
-            energyText = "negative ";
+            energyText = " negative ";
         }
         // Switch button
         String meditate_text = "";
         Switch meditate = (Switch)findViewById(R.id.switch1);
         boolean meditate_button = meditate.isChecked();
         if(meditate_button){
-            meditate_text = "meditates ";
+            meditate_text = " meditates ";
         }
         // Radio button
         String radio_text = "";
@@ -49,35 +52,40 @@ public class MainActivity extends AppCompatActivity {
         int yoga_id = yoga.getCheckedRadioButtonId();
         switch (yoga_id){
             case -1:
-                radio_text = "no ";
+                radio_text = "";
                 break;
             case R.id.radioButton:
-                radio_text = "Yin ";
+                radio_text = " Yin ";
                 break;
             case R.id.radioButton2:
-                radio_text = "Bikram ";
+                radio_text = " Bikram ";
                 break;
             case R.id.radioButton3:
-                radio_text = "Hatha ";
+                radio_text = " Hatha ";
                 break;
             default:
-                radio_text = "no ";
+                radio_text = "";
         }
         // Checkboxes
         String checkbox_text = "";
         CheckBox check1 = (CheckBox)findViewById(R.id.checkBox1);
-        CheckBox check2 = (CheckBox)findViewById(R.id.checkBox2);
         CheckBox check3 = (CheckBox)findViewById(R.id.checkBox3);
         if(check1.isChecked()){
-            checkbox_text += "sarcastic";
-        }
-        if(check2.isChecked()){
-            checkbox_text += "secretive";
+            checkbox_text += " sarcastic";
         }
         if(check3.isChecked()){
-            checkbox_text += "conservative";
+            checkbox_text += " conservative";
         }
+
+        // Images
+        ImageView moodImage = (ImageView)findViewById(R.id.imageView);
+        moodImage.setImageResource(R.drawable.donut);
+
+        // Rating bar
+        RatingBar rate = (RatingBar)findViewById(R.id.ratingBar);
+        int rateStar = rate.getNumStars();
+        String rate_text = " " + String.valueOf(rateStar) + " star ";
         // Show Text
-        feeling.setText(nameValue + " is a" + energyText + spinnerValue + "person " + " that" + meditate_text + radio_text + checkbox_text);
+        feeling.setText(nameValue + " is a" + energyText + spinnerValue + rate_text + "person " + "that " + meditate_text + radio_text + checkbox_text);
     }
 }
